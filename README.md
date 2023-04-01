6f7f2e18bd612abcf6fd9ec6ec53a6897ca5cdde
Henry Booysen <hhbooysen@gmail.com>
2023-04-01 21:17:38 +0200

    Create console-based game that uses only a prompt for user input

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+This game was created with only a javascript file that writes output to the browser console. It uses no GUI.
+The index.html file is only used to host the link to the app.js file that runs the game.
---
+++ app.js
@@ -0,0 +1,63 @@
+
+game();
+
+function game() {
+    let counter = 1;
+    let gameOver = false;
+    let playerChoice;
+    let playerScore = 0;
+    let computerScore = 0;
+    while (!gameOver) {
+        playerChoice = prompt('Please enter your choice of the following options:"Rock", "Paper", "Scissors');
+        let computerChoice = getComputerChoice();
+        console.log('computer choice:', computerChoice);
+        
+        let result = playRound(playerChoice, computerChoice);
+        console.log(result);
+        playerScore += +result[1];
+        computerScore += +result[2];
+        console.log('playerScore:', playerScore);
+        console.log('computerScore:', computerScore);
+        counter++;
+        if (counter === 6) {
+            gameOver = true;
+            if (playerScore > computerScore) {
+                console.log(`You win! Your score: ${playerScore} - computer's score: ${computerScore}`);
+            } else if (playerScore < computerScore) {
+                console.log(`You lose! Your score: ${playerScore} - computer's score: ${computerScore}`);
+            } else if (playerScore === computerScore) {
+                console.log(`It's a draw! Your score: ${playerScore} - computer's score: ${computerScore}`);
+            }
+        }
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    let result;
+    if (playerSelection === computerSelection) {
+        result = [`It's a tie! ${playerSelection} equals ${computerSelection}`,0 ,0];
+    } else if ((playerSelection === 'rock') && (computerSelection === 'scissors')) {
+        result = ['You win! Rock beats Scissors', 1, 0];
+    } else if ((playerSelection === 'paper') && (computerSelection === 'rock')) {
+        result = ['You win! Paper beats Rock', 1, 0];
+    } else if ((playerSelection === 'scissors') && (computerSelection === 'paper')) {
+        result = ['You win! Scissors beats Paper', 1, 0];
+    } else if ((playerSelection === 'scissors') && (computerSelection === 'rock')) {
+        result = ['You lose! Rock beats Scissors', 0, 1];
+    } else if ((playerSelection === 'rock') && (computerSelection === 'paper')) {
+        result = ['You lose! Paper beats Rock', 0, 1];
+    } else if ((playerSelection === 'paper') && (computerSelection === 'scissors')) {
+        result = ['You lose! Scissors beats Paper', 0, 1];
+    }
+
+    return result;
+}
+
+function getComputerChoice() {
+    const rpsArray = ['rock', 'paper', 'scissors'];
+    let rndIndex = Math.floor(Math.random() * rpsArray.length);
+    // console.log('index:', rndIndex);
+    let choice = rpsArray[rndIndex];
+    return choice;
+}
